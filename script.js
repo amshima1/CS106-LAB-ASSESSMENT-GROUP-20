9c83ee2d4242f434ef332821c826d93af43ee07d
amshima1 <v.amshima9021@miva.edu.ng>
2026-04-02 08:05:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const navClose = document.getElementById('nav-close');
     const sideMenu = document.getElementById('side-menu');
 
-    // 1. Open the menu
+    // Slide out menu
     if (navOpen) {
         navOpen.addEventListener('click', () => {
             sideMenu.classList.add('active');
         });
     }
 
-    // 2. Close the menu
+    // Hide menu
     if (navClose) {
         navClose.addEventListener('click', () => {
             sideMenu.classList.remove('active');
         });
     }
 
-    // 3. Close if a link is clicked
+    // Close menu when a link is clicked
     const links = document.querySelectorAll('.nav-links a');
     links.forEach(link => {
         link.addEventListener('click', () => {
